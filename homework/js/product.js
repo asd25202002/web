9a4de8dac2cabdaa5f8b774b7e43b38e53ed66fb
Yui <asd25202002@gmail.com>
2020-08-18 10:15:19 +0800

    更新product
    產品圖片 js css 網頁內容

---
+++ homework/js/product.js
@@ -0,0 +1,10 @@
+var oBtn1=document.getElementById("btn1");
+var oBtn2=document.getElementById("btn2");
+oBtn1.onclick=function(){
+var oTxt=document.getElementById("txt1");
+oTxt.value="good!";
+};
+oBtn2.onclick=function(){
+var oTxt=document.getElementById("txt1");
+oTxt.value="";
+};
