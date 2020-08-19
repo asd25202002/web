ddf07530a0aa720323f5bf4063ea954abda57ee0
Yui <asd25202002@gmail.com>
2020-08-19 11:33:38 +0800

    更新網頁
    圖片輪播

--- homework/js/product.js
+++ homework/js/product.js
@@ -1,22 +1,18 @@
 var oBtn1=document.getElementById("btn1");
 var oBtn2=document.getElementById("btn2");
-var p_img = document.getElementById("p_img");
-var img=document.getElementsByTagName("p_img")[0];
 var imgarr=["Images/product1.png","Images/product2.png","Images/product3.jpg","Images/product4.jpg","Images/product5.png"];
 var index=0;
 oBtn1.onclick=function(){
-    var oTxt=document.getElementById("p_txt").innerHTML="good!";
+    
     index--;//切換到上一張索引自減
     //判斷索引是否小於0
     if(index<0)
     {
         index=imgarr.length-1;//目的是可以實現迴圈切換
     }
-    //修改img的src屬性
-    p_img.src=imgarr[index];
+    var oTxt=document.getElementById("p_txt").innerHTML=imgarr[index];
 };
 oBtn2.onclick=function(){
-    var oTxt=document.getElementById("p_txt").innerHTML="";
     //書寫事件驅動程式
     index++;//切換到下一張索引自加
     //判斷索引是否大於4
@@ -24,6 +20,5 @@ oBtn2.onclick=function(){
     {
         index=0;//目的是可以實現迴圈切換
     }
-    //修改img的src屬性
-    p_img.src=imgarr[index];
+    var oTxt=document.getElementById("p_txt").innerHTML=imgarr[index];
 };
