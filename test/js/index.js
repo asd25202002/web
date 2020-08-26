4234354a7a717d0635e1204f005664f94d54d01c
Yui <asd25202002@gmail.com>
2020-08-26 10:08:17 +0800

    更新網頁
    test

--- test/js/index.js
+++ test/js/index.js
@@ -1,11 +1,19 @@
-$(function(){
-    $(window).load(function(){
-        $(window).bind('scroll resize', function(){
-            var $this = $(this);
-            var $this_Top=$this.scrollTop();
-            if($this_Top == 0){
-                   $('p').addClass('p')
-            }
-        }).scroll();
-    });
+// $(function(){
+//     $(window).load(function(){
+//         $(window).bind('scroll resize', function(){
+//             var $this = $(this);
+//             var $this_Top=$this.scrollTop();
+//             if($this_Top == 0){
+                   
+//             }
+//         }).scroll();
+//     });
+// });
+
+window.addEventListener("scroll",(event)=>{
+    var y = this.scrollY;
+    if(y >= 100){
+        console.log('安安');
+        $('#p').addClass('p');
+    }
 });
